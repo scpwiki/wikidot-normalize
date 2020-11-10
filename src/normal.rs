c81620dedda714f544c1bd62ba63deb62f38644e
Ammon Smith <ammon.i.smith@gmail.com>
2020-11-10 02:13:19 -0500

    Implement replace_underscores.

--- src/normal.rs
+++ src/normal.rs
@@ -76,6 +76,49 @@ pub fn is_normal(name: &str) -> bool {
 
 // Helpers
 
+/// Manual implementation of the PCRE "(?<!:)_", which uses a negative look-behind.
+///
+/// Instead of a somewhat messy standard regex to replace it, it's easier
+/// (and conveys the intent better) if I have a regular function do it instead.
+///
+/// This looks for underscores, provided it is not immediately preceded with a colon.
+/// If such an underscore is found, it is replaced with a dash.
+/// The goal is to replace underscores, excepting leading underscores for pages like
+/// `_template`. The colon rule permits them to exist on categories, like `fragment:_template`.
+///
+/// Wikidot originally achieves this by prepending a colon to implicit `_default` category
+/// slugs, making it avoid the underscore replacement.
+///
+/// That's dumb. This logic just makes an exception for the start of the string instead.
+fn replace_underscores(text: &mut String) {
+    let mut matches = vec![];
+    let mut prev_colon = false;
+
+    // Finding matching, non-conforming underscores
+    for (idx, ch) in text.chars().enumerate() {
+        if ch == '_' {
+            // Allow a leading underscore at the start
+            if idx == 0 {
+                continue;
+            }
+
+            // Allow a leading underscore after a category
+            if prev_colon {
+                continue;
+            }
+
+            matches.push(idx);
+        }
+
+        prev_colon = ch == ':';
+    }
+
+    // Replace them with dashes
+    for idx in matches {
+        text.replace_range(idx..idx+1, "-");
+    }
+}
+
 fn replace_in_place(text: &mut String, regex: &Regex, replace_with: &str) {
     use regex::Captures;
     use std::ops::Range;
