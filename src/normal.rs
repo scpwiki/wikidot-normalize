91694b37b5575556edaef07569fbaaff9ac2194f
Ammon Smith <ammon.i.smith@gmail.com>
2021-03-30 20:41:35 -0400

    Fix conversion test case.

--- src/normal.rs
+++ src/normal.rs
@@ -156,7 +156,7 @@ fn test_normalize() {
     check!("Template_", "template");
     check!("Template__", "template");
     check!(" <[ TEST ]> ", "test");
-    check!("ÄÀ-áö ðñæ_þß*řƒŦ", "aa-aoe-dnae-tss-rft");
+    check!("ÄÀ-áö ðñæ_þß*řƒŦ", "aa-ao-dnae-thss-rft");
     check!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "");
     check!("Component:image block", "component:image-block");
     check!("fragment:scp-4447-2", "fragment:scp-4447-2");
